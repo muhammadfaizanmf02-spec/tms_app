0d5f7c52cc8f246c4e51b1d5ce3e29f2620acef5
MUHAMMAD FAIZAN <muhammadfaizan.mf02@gmail.com>
2026-09-03 16:24:09 +0500

    Delete tms_app/tms_app/tms/doctype/seat_layout/seat_layout.py

--- tms_app/tms_app/tms/doctype/seat_layout/seat_layout.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Your Company and contributors
-# For license information, please see license.txt
-
-from frappe.model.document import Document
-
-
-class SeatLayout(Document):
-  	pass
-  
